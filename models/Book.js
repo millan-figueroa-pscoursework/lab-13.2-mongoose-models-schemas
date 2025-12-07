9e9c66b79f92326fa4531b3c9570bd8d1481a91b
millan-figueroa <millan.fig@gmail.com>
2025-12-07 00:01:25 -0800

    Updates formatting for Book.js and imports express

--- models/Book.js
+++ models/Book.js
@@ -1,27 +1,32 @@
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-    title:{
-        type: String,
-        required: true,
-        trim: true
-    },
-    author:{
-        type: String,
-        required: true,
-        trime: true
-    },
-    isbn:{
-        type:String,
-        unique: true
-    },
-    publishedDate:{
-        type: Date
-    },
-    inStock:{
-        type: Boolean,
-        default: true
-    }
-})
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  author: {
+    type: String,
+    required: true,
+    trime: true,
+  },
+
+  isbn: {
+    type: String,
+    unique: true,
+  },
+
+  publishedDate: {
+    type: Date,
+  },
+
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+});
 
 module.exports = mongoose.model("Book", bookSchema);
